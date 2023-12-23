16703d92ef2c6ab6d8d6844887d8f62ab0239d14
Albirth <albertmirasoljr@gmail.com>
2023-12-23 21:28:16 +0800

    simple and complex select statement

--- api.py
+++ api.py
@@ -33,6 +33,30 @@ def get_addresses():
     return make_response(jsonify(data), 200)
 
 
+@app.route("/addresses/search/<int:id>", methods=["GET"])
+def get_addresses_by_id(id):
+    data = data_fetch("""SELECT * FROM maritime.addresses where Address_ID = %s""", (id,))
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/clients/<int:id>/addresses", methods=["GET"])
+def get_addresses_by_clients(id):
+    data = data_fetch(
+        """
+        SELECT addresses.City, addresses.Country 
+        FROM addresses 
+        INNER JOIN clients
+        ON addresses.Address_ID = clients.Client_ID 
+        INNER JOIN ref_client_categories
+        ON clients.Client_ID  = ref_client_categories.Client_Category_Code 
+        WHERE addresses.Address_ID = {}
+    """.format(
+            id
+        )
+    )
+    return make_response(
+        jsonify({"Address_ID": id, "count": len(data), "addresses": data}), 200
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
